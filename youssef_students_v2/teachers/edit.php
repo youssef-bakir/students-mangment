7c9983b4185a888785bf7ee3f5470df84901086c
Youssef Bakir <youssebakir19@gmail.com>
2025-08-20 20:47:13 +0300

    Update edit.php

--- youssef_students_v2/teachers/edit.php
+++ youssef_students_v2/teachers/edit.php
@@ -3,7 +3,7 @@ include '../includes/header.php';
 include '../config/db.php';
 
 $id = $_GET['id'];
-$sql = "SELECT * FROM teachers WHERE id = $id AND added_by = {$_SESSION['user_id']}";
+$sql = "SELECT * FROM teachers ";
 $result = $conn->query($sql);
 $teacher = $result->fetch_assoc();
 
@@ -43,4 +43,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
