013166a89b9845f3783e55be35e6c09e78e5d2a6
Youssef Bakir <youssebakir19@gmail.com>
2025-08-20 21:03:34 +0300

    Update edit.php

--- youssef_students_v2/subjects/edit.php
+++ youssef_students_v2/subjects/edit.php
@@ -3,7 +3,7 @@ include '../includes/header.php';
 include '../config/db.php';
 
 $id = $_GET['id'];
-$sql = "SELECT * FROM subjects";
+$sql = "SELECT * FROM subjects WHERE id = $id ";
 $result = $conn->query($sql);
 $subject = $result->fetch_assoc();
 
@@ -33,5 +33,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
