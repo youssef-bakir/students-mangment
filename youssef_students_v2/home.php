7abc825d34a21ccca5e25166e0e8e251c5eddba6
Youssef Bakir <youssebakir19@gmail.com>
2025-08-20 20:19:42 +0300

    Update home.php

--- youssef_students_v2/home.php
+++ youssef_students_v2/home.php
@@ -1,35 +1,28 @@
 <?php include 'includes/header.php'; ?>
 <div class="container mt-5"><div class="text-center mt-5">
-  <div id="carouselExampleCaptions" class="carousel slide">
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+  
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
+  
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="images/erika-fletcher-MZxqc6n9qCw-unsplash.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+      <div class="carousel-caption d-none d-md-block"></div>
     </div>
     <div class="carousel-item">
       <img src="images/cdc-GDokEYnOfnE-unsplash.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
+      <div class="carousel-caption d-none d-md-block"></div>
     </div>
     <div class="carousel-item">
       <img src="images/nathan-cima-Qw6wa96IvvQ-unsplash.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
+      <div class="carousel-caption d-none d-md-block"></div>
     </div>
-    
   </div>
+  
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
@@ -41,23 +34,55 @@
 </div>
 </div></div>
 <br>
-<div class="row">
-  <div class="card" style="width: 18rem;">
-  <img src="images/andrew-ebrahim-zRwXf6PizEo-unsplash.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">جدول الطلاب </h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+
+<div class="container mt-5">
+  <div class="row g-4">
+
+    <div class="col-md-3">
+      <div class="card h-100 shadow">
+        <img src="https://img.icons8.com/fluency/300/student-male.png" class="card-img-top" alt="طلاب">
+        <div class="card-body text-center">
+          <h5 class="card-title">الطلاب</h5>
+          <p class="card-text">إدارة بيانات الطلاب، متابعة حضورهم ودرجاتهم وتفاصيلهم الشخصية.</p>
+          <a href="students/view.php" class="btn btn-primary">عرض الطلاب</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <div class="card h-100 shadow">
+        <img src="https://img.icons8.com/fluency/300/teacher.png" class="card-img-top" alt="معلمين">
+        <div class="card-body text-center">
+          <h5 class="card-title">المعلمين</h5>
+          <p class="card-text">إدارة بيانات المعلمين، المواد التي يدرسونها ومتابعة جداولهم.</p>
+          <a href="teachers/view.php" class="btn btn-success">عرض المعلمين</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <div class="card h-100 shadow">
+        <img src="https://img.icons8.com/fluency/300/classroom.png" class="card-img-top" alt="كلاسات">
+        <div class="card-body text-center">
+          <h5 class="card-title">الكلاسات</h5>
+          <p class="card-text">متابعة وإدارة الفصول الدراسية، توزيع الطلاب والمعلمين على الكلاسات.</p>
+          <a href="classes/view.php" class="btn btn-warning">عرض الكلاسات</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <div class="card h-100 shadow">
+        <img src="https://img.icons8.com/fluency/300/books.png" class="card-img-top" alt="مواد دراسية">
+        <div class="card-body text-center">
+          <h5 class="card-title">المواد الدراسية</h5>
+          <p class="card-text">إدارة المواد الدراسية، متابعة المناهج والمحتوى الدراسي المقدم.</p>
+          <a href="subjects/view.php" class="btn btn-danger">عرض المواد</a>
+        </div>
+      </div>
+    </div>
+
   </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="images/2y-kang-dFohf_GUZJ0-unsplash.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
