34748dd8cc3954dca124082ed51550b8175e13a5
Youssef Bakir <youssebakir19@gmail.com>
2025-08-20 21:04:01 +0300

    Update delete.php

--- youssef_students_v2/subjects/delete.php
+++ youssef_students_v2/subjects/delete.php
@@ -3,7 +3,7 @@ include '../includes/header.php';
 include '../config/db.php';
 
 $id = $_GET['id'];
-$sql = "DELETE FROM subjects WHERE id = $id AND added_by = {$_SESSION['user_id']}";
+$sql = "DELETE FROM subjects WHERE id = $id ";
 $conn->query($sql);
 header("Location: view.php");
 ?>
